f52520adf159af33cd0f21d55a1aa0382feb355a
internet_trash <naiknischal92@gmail.com>
2024-01-25 21:20:51 +0530

    fixed hand detection

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef , useEffect} from 'react';
 import * as tf from '@tensorflow/tfjs';
 import * as handpose from '@tensorflow-models/handpose';
 import Webcam from 'react-webcam';
@@ -10,18 +10,53 @@ function App() {
   const runhandpose = async () => {
     const net = await handpose.load();
     console.log('model load ho gaya ');
+    setInterval(() => {
+      detect(net);
+    }, 100);
+  };
+  useEffect(() => {
+    runhandpose();
+  }, []);
+
+
+  const detect = async (net) => {
+    if(typeof Webcamref.current !== "undefined" && Webcamref.current !== null && Webcamref.current.video.readyState === 4){
+     //get the video properties
+      const video = Webcamref.current.video;
+      const videoWidth = Webcamref.current.video.videoWidth;
+      const videoHeight = Webcamref.current.video.videoHeight;
+
+      //set video properties
+      Webcamref.current.video.width = videoWidth;
+      Webcamref.current.video.height = videoHeight;
+
+      // doing same for canvas
+      canvasref.current.width = videoWidth;
+      canvasref.current.height = videoHeight;
+
+      //making detections
+      const hand = await net.estimateHands(video);
+      if (hand == 0) {
+        console.log("tera haat nai  dik raha ");}else{
+          console.log("tera haat dik raha ");
+        }
+
+      //console.log(hand);
+     
+
+      // render the mesh 
+      // const ctx =  canvasref.current.getContext("2d");
+      // drawHand(hand, ctx);
+    }
   };
 
   return (
     <>
       <div  className='App'>
         <header className='App-header'/>
-          <h1>Hand Pose Detection</h1>
+          <h1>tera haat dik raa?</h1>
           <canvas ref={canvasref} style={webcamstyle}/>
-        <Webcam  ref={Webcamref}style={webcamstyle}/>
-      
-
-      
+        <Webcam  ref={Webcamref} style={webcamstyle}/>
       </div>
     </>
   );
@@ -37,8 +72,6 @@ const webcamstyle = {
   zindex: 9,
   width: 640,
   height: 480,
-
-  
 };
 
 export default App;
