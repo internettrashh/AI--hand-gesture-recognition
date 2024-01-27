577e10baff6bb10d59824a7fd5ddc0985b925613
internet_trash <naiknischal92@gmail.com>
2024-01-27 16:34:45 +0530

    fixed sizzor gesture

--- src/Fingagestures/Sizzor.js
+++ src/Fingagestures/Sizzor.js
@@ -7,23 +7,21 @@ import {Finger, FingerCurl, FingerDirection, GestureDescription} from 'fingerpos
 
 
 // thumb:
-victoryDescription.addDirection(Finger.Thumb, FingerDirection.VerticalUp, 1.0);
+//victoryDescription.addDirection(Finger.Thumb, FingerDirection.VerticalUp, 1.0);
 victoryDescription.addDirection(Finger.Thumb, FingerDirection.DiagonalUpLeft, 1.0);
 victoryDescription.addDirection(Finger.Thumb, FingerDirection.DiagonalUpRight, 1.0);
 
 // index:
 victoryDescription.addCurl(Finger.Index, FingerCurl.NoCurl, 1.0);
-victoryDescription.addDirection(Finger.Index, FingerDirection.VerticalUp, 1.0);
-victoryDescription.addDirection(Finger.Index, FingerDirection.DiagonalUpLeft, 1.0);
-victoryDescription.addDirection(Finger.Index, FingerDirection.DiagonalUpRight, 1.0);
+//victoryDescription.addDirection(Finger.Index, FingerDirection.VerticalUp, 1.0);
+
 victoryDescription.addDirection(Finger.Index, FingerDirection.HorizontalLeft, 1.0);
 victoryDescription.addDirection(Finger.Index, FingerDirection.HorizontalRight, 1.0);
 
 // middle:
 victoryDescription.addCurl(Finger.Middle, FingerCurl.NoCurl, 1.0);
-victoryDescription.addDirection(Finger.Middle, FingerDirection.VerticalUp, 1.0);
-victoryDescription.addDirection(Finger.Middle, FingerDirection.DiagonalUpLeft, 1.0);
-victoryDescription.addDirection(Finger.Middle, FingerDirection.DiagonalUpRight, 1.0);
+//victoryDescription.addDirection(Finger.Middle, FingerDirection.VerticalUp, 1.0);
+
 victoryDescription.addDirection(Finger.Middle, FingerDirection.HorizontalLeft, 1.0);
 victoryDescription.addDirection(Finger.Middle, FingerDirection.HorizontalRight, 1.0);
 
